52abc742c90843599dce9cccfb4aa62d85e89493
Chris <chris.elies13@gmail.com>
2019-01-14 20:32:21 +0100

    feat(): created delegate protocol and protocol

--- Viper.xctemplate/FileHeaderComments/___FILEBASENAME___Presenter.swift
+++ Viper.xctemplate/FileHeaderComments/___FILEBASENAME___Presenter.swift
@@ -20,12 +20,14 @@ final class ___VARIABLE_MODULENAME___Presenter: ___VARIABLE_MODULENAME___Present
     private let interactor: ___VARIABLE_MODULENAME___InteractorProtocol
     private let router: ___VARIABLE_MODULENAME___RouterProtocol
     private let dependencies: ___VARIABLE_MODULENAME___PresenterDependenciesProtocol
+    private weak var delegate: ___VARIABLE_MODULENAME___DelegateProtocol?
     
-    init(dependencies: ___VARIABLE_MODULENAME___PresenterDependenciesProtocol, view: ___VARIABLE_MODULENAME___ViewProtocol, interactor: ___VARIABLE_MODULENAME___InteractorProtocol, router: ___VARIABLE_MODULENAME___RouterProtocol) {
+    init(dependencies: ___VARIABLE_MODULENAME___PresenterDependenciesProtocol, view: ___VARIABLE_MODULENAME___ViewProtocol, interactor: ___VARIABLE_MODULENAME___InteractorProtocol, router: ___VARIABLE_MODULENAME___RouterProtocol, delegate: ___VARIABLE_MODULENAME___DelegateProtocol?) {
         self.dependencies = dependencies
         self.view = view
         self.interactor = interactor
         self.router = router
+        self.delegate = delegate
     }
     
     func viewDidLoad() {
