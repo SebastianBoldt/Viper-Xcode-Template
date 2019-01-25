6157b43ed1553c432184dc47f39b079bcba22abf
Chris <chris.elies13@gmail.com>
2019-01-25 16:48:02 +0100

    feat(): view should be a UIViewController because it is most of the time, created router dependencies

--- Viper.xctemplate/Default/___FILEBASENAME___Router.swift
+++ Viper.xctemplate/Default/___FILEBASENAME___Router.swift
@@ -5,9 +5,11 @@ protocol ___VARIABLE_MODULENAME___RouterProtocol {
 }
 
 final class ___VARIABLE_MODULENAME___Router: ___VARIABLE_MODULENAME___RouterProtocol {
+    private let dependencies: ___VARIABLE_MODULENAME___RouterDependenciesProtocol
     private weak var viewController: UIViewController?
 
-    init(viewController: UIViewController?) {
+    init(dependencies: ___VARIABLE_MODULENAME___RouterDependenciesProtocol, viewController: UIViewController?) {
+        self.dependencies = dependencies
     	self.viewController = viewController
     }
 }
